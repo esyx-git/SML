18d9917e464174ae78276c3595a03ea490ae6af6
Esyx <53837034+esyx-git@users.noreply.github.com>
2023-10-15 00:41:03 -0700

    Add files via upload
    Updated functionality.

--- README.txt
+++ README.txt
@@ -1,25 +1,49 @@
-This is a simplistic launcher for Skyrim that is designed to automatically
-open, monitor, and close Mod Organizer, Mantella, and xVASynth.
+# Simple Mantella Launcher (SML)
 
-I created this application after getting tired of opening and
-closing each of these programs individually.
+SML is a simple and convenient launcher for Skyrim that is designed to automatically open, monitor, and close Mod Organizer, Mantella, and xVASynth. (There is also support for a self-hosted LLM and Herika as well.)
+It eliminates the need to open and close each program individually, providing a seamless experience for you, the user.
 
-Before using this launcher, you must specify the path to each program
-within the provided Config.xml file.
+## Getting Started
 
-To do this, open the Config.xml file and change the paths.
+Before using SML, you must specify the path to each program within the provided Config.xml file. Follow the steps below to set up the launcher correctly:
 
-******EXAMPLE*******
-<Program>C:\Path\To\Program.exe</Program>
+1. Open the Config.xml file using a text editor.
+2. Locate the `<Program>` tags for each required program (Mod Organizer, Mantella, and xVASynth).
+3. Update the path within each `<Program>` tag to point to the respective program's executable file on your computer.
+   - For example: `<ModOrganizer>C:\Path\To\ModOrganizer.exe</ModOrganizer>`
+4. If you are using LLM and/or Herika, set the respected <UseProgram> tag to true and set the correct path, otherwise ignore this step. (LLM and Herika paths can be left alone if their tags are set to false.)
+   - For example: '<LLM>true</LLM>'
+5. Save the Config.xml file.
 
-After setting the path for each program, save the Config.xml file.
+**Important:** Make sure that both the Config.xml file and the Skyrim Launcher executable file are located in the same folder.
 
-*****IMPORTANT****** 
-Make sure the Config.xml and .exe files are in the same folder!
+## Running the Launcher
 
-You can now run the launcher.
+To run SML, simply execute the launcher's executable file. 
 
-Create a shortcut for it and enjoy!
+Optional steps for convenience:
 
-P.S. To close everything, just close Mod Organizer.
-(The launcher will do the rest.)
+1. Create a shortcut to the launcher executable file for easy access.
+2. Customize the shortcut's icon to differentiate it from other shortcuts.
+3. Optionally, pin the shortcut to your start menu for quicker access.
+4. Profit?
+
+## Closing the Programs
+
+To close all opened programs, simply close Mod Organizer. SML will automatically close all associated programs (Mantella, and xVASynth) as well.
+
+**Note:** SML will only open and close additional programs (LLM and Herika) if the respective options in the Config.xml file are set to true.
+
+## Troubleshooting
+
+If you encounter any issues or have questions, please feel free to reach out for support.
+
+**Note** The launcher may not close associated programs properly if they are manually opened and closed, but this should not have any catastrophic results other than a mild inconveniece.
+
+## License
+
+This project is licensed under the [GPL v3.0 License](https://opensource.org/license/gpl-3-0/) - see the [LICENSE](LICENSE) file for details.
+
+## Acknowledgments
+
+- Mantella developers for their fantastic mod.
--- Source/SML.cs
+++ Source/SML.cs
@@ -1,1 +1,115 @@
-null
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
+using System.Xml;
+
+namespace SML
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.Load("Config.xml");
+
+            string executable1 = doc.SelectSingleNode("/Configuration/ModOrganizer").InnerText;
+            string executable2 = doc.SelectSingleNode("/Configuration/Mantella").InnerText;
+            string executable3 = doc.SelectSingleNode("/Configuration/xVASynth").InnerText;
+            string executable4 = doc.SelectSingleNode("/Configuration/LLM").InnerText;
+            string executable5 = doc.SelectSingleNode("/Configuration/Herika").InnerText;
+
+            string directory1 = Path.GetDirectoryName(executable1);
+            string directory2 = Path.GetDirectoryName(executable2);
+            string directory3 = Path.GetDirectoryName(executable3);
+            string directory4 = Path.GetDirectoryName(executable4);
+            string directory5 = Path.GetDirectoryName(executable5);
+
+            bool useExecutable4 = bool.Parse(doc.SelectSingleNode("/Configuration/UseLLM").InnerText);
+            bool useExecutable5 = bool.Parse(doc.SelectSingleNode("/Configuration/UseHerika").InnerText);
+
+
+            Console.WriteLine("Opening Mod Organizer.");
+
+            ProcessStartInfo startInfo = new ProcessStartInfo(executable1);
+            startInfo.WorkingDirectory = directory1;
+            Process mainProcess = Process.Start(startInfo);
+
+            Console.WriteLine("Watching Mod Organizer process.");
+
+            Console.WriteLine("Opening Mantella and xVASynth.");
+
+            ProcessStartInfo startInfo2 = new ProcessStartInfo(executable2);
+            startInfo2.WorkingDirectory = directory2;
+            startInfo2.WindowStyle = ProcessWindowStyle.Minimized;
+            Process process2 = Process.Start(startInfo2);
+
+            ProcessStartInfo startInfo3 = new ProcessStartInfo(executable3);
+            startInfo3.WorkingDirectory = directory3;
+            startInfo3.WindowStyle = ProcessWindowStyle.Minimized;
+            Process process3 = Process.Start(startInfo3);
+
+            Console.WriteLine("Watching Mantella and xVASynth processes.");
+
+            if (useExecutable4)
+            {
+                Console.WriteLine("Opening LLM.");
+
+                ProcessStartInfo startInfo4 = new ProcessStartInfo(executable4);
+                startInfo4.WorkingDirectory = directory4;
+                startInfo4.WindowStyle = ProcessWindowStyle.Minimized;
+                Process process4 = Process.Start(startInfo4);
+
+                Console.WriteLine("Watching LLM process.");
+
+                mainProcess.WaitForExit();
+
+                if (!process4.HasExited)
+                {
+                    process4.CloseMainWindow();
+                    process4.WaitForExit();
+                }
+            }
+
+            if (useExecutable5)
+            {
+
+                Console.WriteLine("Opening Herika.");
+
+                ProcessStartInfo startInfo5 = new ProcessStartInfo(executable5);
+                startInfo5.WorkingDirectory = directory5;
+                startInfo5.WindowStyle = ProcessWindowStyle.Minimized;
+                Process process5 = Process.Start(startInfo5);
+
+                Console.WriteLine("Watching Herika process.");
+
+                mainProcess.WaitForExit();
+
+                if (!process5.HasExited)
+                {
+                    process5.CloseMainWindow();
+                    process5.WaitForExit();
+                }
+            }
+
+            mainProcess.WaitForExit();
+
+            Console.WriteLine("Mod Organizer has closed. Now closing other applications.");
+
+            if (!process2.HasExited)
+            {
+                process2.CloseMainWindow();
+                process2.WaitForExit();
+            }
+
+            if (!process3.HasExited)
+            {
+                process3.CloseMainWindow();
+                process3.WaitForExit();
+            }
+
+            Console.WriteLine("\nSuccess! This window will close in 3 seconds...");
+            Thread.Sleep(3000);
+        }
+    }
+}
